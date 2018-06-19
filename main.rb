dc3d548b16fe33ac878a7d103d9587739a77bf39
nim_13515118 <gianffh@gmail.com>
2018-06-20 01:15:28 +0700

    Add three flags handling in main program

--- main.rb
+++ main.rb
@@ -6,7 +6,9 @@ flags = ARGV
 case flags.length
 when 0
   map = Map.new
+
   drivers = []
+  #Generate 5 drivers
   for i in 1..5
     posX = Random.rand(0...map.size)
     posY = Random.rand(0...map.size)
@@ -15,9 +17,35 @@ when 0
     map.insert(driver)
   end
 
+  #init a user
   posX = Random.rand(0...map.size)
   posY = Random.rand(0...map.size)
   user = User.new(posX, posY)
+  map.insert(user)
+
+when 3
+  size = flags[0].to_i
+  userPosX = flags[1].to_i
+  userPosY = flags[2].to_i
+
+  if userPosY < 0 || userPosY >= size || userPosX < 0 || userPosX >= size
+    puts "Wrong input : user's position is out of boundary"
+    Kernel.exit(false)
+  end
+
+  map = Map.new(size)
+
+  drivers = []
+  #Generate 5 drivers 
+  for i in 1..5
+    posX = Random.rand(0...map.size)
+    posY = Random.rand(0...map.size)
+    driver = Driver.new(posX, posY, "Driver" + i.to_s)
+    drivers << driver
+    map.insert(driver)
+  end
+
+  user = User.new(userPosX, userPosY)
   map.insert(user)
 end
 
@@ -27,7 +55,7 @@ while true do
   puts "2. Order Go-Ride"
   puts "3. View History"
   print "Please pick an action > "
-  option = gets.to_i
+  option = $stdin.gets.chomp.to_i
   case option
   when 1
     map.show
